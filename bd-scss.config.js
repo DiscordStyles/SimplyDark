6b11e1832486ef09b5a6f11efed3a8ed0362a6dc
Ian <Gibbu_@hotmail.com>
2022-10-27 11:04:26 +1100

    Finally updated

---
+++ bd-scss.config.js
@@ -0,0 +1,16 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: 'SimplyDark',
+		author: 'Gibbu#1211',
+		version: '3.0.0',
+		description: 'A sharp and simple theme for Discord',
+		source: 'https://github.com/DiscordStyles/SimplyDark',
+		invite: 'ZHthyCw',
+		website: 'https://gibbu.me/'
+	},
+	base: {
+		output: 'dist/dist'
+	},
+	baseImport: 'https://discordstyles.github.io/SoftX/SoftX.css',
+}
